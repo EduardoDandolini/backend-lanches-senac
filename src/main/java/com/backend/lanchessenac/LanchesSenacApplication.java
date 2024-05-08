1d128be12c00b07ddd88adcf0dc82ab83dbe47ab
Eduardo <eduardodasilvadandolini@gmail.com>
2024-05-08 20:28:03 -0300

    first commit

---
+++ src/main/java/com/backend/lanchessenac/LanchesSenacApplication.java
@@ -0,0 +1,13 @@
+package com.backend.lanchessenac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanchesSenacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanchesSenacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/lanchessenac/LanchesSenacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.lanchessenac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanchesSenacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
