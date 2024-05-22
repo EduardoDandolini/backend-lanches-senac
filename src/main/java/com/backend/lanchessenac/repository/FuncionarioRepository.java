dd27924405695979d0210fc011652774f374d54e
rafael <rafael.florianoc9@gmail.com>
2024-05-22 19:47:36 -0300

    FEAT: implementado a query 'existsByNomeIgnoreCase' para realizar a checagem no banco se já existe um funcionário com nome passado no parametro

--- src/main/java/com/backend/lanchessenac/repository/FuncionarioRepository.java
+++ src/main/java/com/backend/lanchessenac/repository/FuncionarioRepository.java
@@ -2,6 +2,10 @@ package com.backend.lanchessenac.repository;
 
 import com.backend.lanchessenac.entity.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+    @Query("select (count(f) > 0) from Funcionario f where upper(f.nome) = upper(:nome)")
+    boolean existsByNomeIgnoreCase(@Param("nome") String nome);
 }
