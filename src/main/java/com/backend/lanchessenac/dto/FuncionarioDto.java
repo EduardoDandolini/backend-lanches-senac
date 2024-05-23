993a8c2bcde93f544b798b4223213747db846a8a
rafael <rafael.florianoc9@gmail.com>
2024-05-22 21:55:15 -0300

    FEAT: added new attribute idFuncionario in Dto

--- src/main/java/com/backend/lanchessenac/dto/FuncionarioDto.java
+++ src/main/java/com/backend/lanchessenac/dto/FuncionarioDto.java
@@ -3,4 +3,4 @@ package com.backend.lanchessenac.dto;
 import com.backend.lanchessenac.enums.CargoEnum;
 import jakarta.validation.constraints.NotNull;
 
-public record FuncionarioDto (@NotNull String nome,@NotNull String cpf,@NotNull CargoEnum cargo) {}
+public record FuncionarioDto (Long idFuncionario, @NotNull String nome, @NotNull String cpf, @NotNull CargoEnum cargo) {}
